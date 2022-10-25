f19b0eaa205d44f979f2f5fb99d464c6f60d2002
Andrii Banduliak <banduliak1303@gmail.com>
2022-10-25 21:09:57 +0300

    done

---
+++ bubble_sort.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter the number of array elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Enter array values: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Initial array: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
+}
